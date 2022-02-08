ac741a01032bc82796ee4dc22c5673bfecd25867
mirumi <info@milmemo.net>
2022-02-08 17:23:57 +0900

    [clear] : it works fine!

--- src/background.ts
+++ src/background.ts
@@ -1,7 +1,6 @@
 import { app, protocol, BrowserWindow } from 'electron'
 import { createProtocol } from 'vue-cli-plugin-electron-builder/lib'
 import installExtension, { VUEJS3_DEVTOOLS } from 'electron-devtools-installer'
-
 const isDevelopment = process.env.NODE_ENV !== 'prd'
 
 // Scheme must be registered before the app is ready
@@ -12,13 +11,14 @@ protocol.registerSchemesAsPrivileged([
 async function createWindow() {
   // Create the browser window.
   const win = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1800,
+    height: 870,
     webPreferences: {
+      
       // Use pluginOptions.nodeIntegration, leave this alone
       // See nklayman.github.io/vue-cli-plugin-electron-builder/guide/security.html#node-integration for more info
       nodeIntegration: (process.env.ELECTRON_NODE_INTEGRATION as unknown) as boolean,
-      contextIsolation: !((process.env.ELECTRON_NODE_INTEGRATION as unknown) as boolean as unknown) as boolean
+      contextIsolation: !process.env.ELECTRON_NODE_INTEGRATION
     }
   })
 
@@ -56,7 +56,7 @@ app.on('ready', async () => {
     // Install Vue Devtools
     try {
       await installExtension(VUEJS3_DEVTOOLS)
-    } catch (e: any) {
+    } catch (e: any) {  //eslint-disable-line
       console.error('Vue Devtools failed to install:', e.toString())
     }
   }
